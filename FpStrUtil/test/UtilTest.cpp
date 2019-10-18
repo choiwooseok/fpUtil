9096e11f9fb30df284a4a7ea8451a6e8e1a30148
wschoi <wschoi@192.168.1.42>
2019-10-18 15:25:40 +0900

    test 추가

--- FpStrUtil/test/UtilTest.cpp
+++ FpStrUtil/test/UtilTest.cpp
@@ -5,7 +5,47 @@ TEST(StringUtilTest, stringUtilTest) {
     using namespace fpstr;
 
     EXPECT_EQ(StringUtil::toUpper("asdf"), "ASDF");
+    EXPECT_EQ(StringUtil::toUpper("asDF"), "ASDF");
 
+    EXPECT_EQ(StringUtil::toLower("ASdF"), "asdf");
+    EXPECT_EQ(StringUtil::toLower("ASDF"), "asdf");
+
+    EXPECT_EQ(StringUtil::capitalize("asdf"),"Asdf");
+
+    EXPECT_EQ(StringUtil::isEmpty(""), true);
+    EXPECT_EQ(StringUtil::isEmpty("a"), false);
+
+    EXPECT_EQ(StringUtil::isNotEmpty(""), false);
+    EXPECT_EQ(StringUtil::isNotEmpty("a"), true);
+
+    EXPECT_EQ(StringUtil::makeZeroPrependDigit(13LL, 4), "0013");
+
+    EXPECT_EQ(StringUtil::getAddressToIP("0.0.0.0:1024"), "0.0.0.0");
+    EXPECT_EQ(StringUtil::getAddressToPort("0.0.0.0:1024"), "1024");
+
+    EXPECT_EQ(StringUtil::isAlnum("\xdf"), false);
+    EXPECT_EQ(StringUtil::isAlnum("asdf"), true);
+
+    EXPECT_EQ(StringUtil::isAlpha("asdf"), true);
+    EXPECT_EQ(StringUtil::isAlpha("123a"), false);
+
+    EXPECT_EQ(StringUtil::isDigit("1234"), true);
+    EXPECT_EQ(StringUtil::isDigit("123a"), false);
+
+    EXPECT_EQ(StringUtil::isLower("asdf"), true);
+    EXPECT_EQ(StringUtil::isLower("asDf"), false);
+
+    EXPECT_EQ(StringUtil::isUpper("ASDF"), true);
+    EXPECT_EQ(StringUtil::isUpper("asDf"), false);
+
+    EXPECT_EQ(StringUtil::isSpace(" "), true);
+    EXPECT_EQ(StringUtil::isSpace("a"), false);
+
+    EXPECT_EQ(StringUtil::substringBefore("zxcv", "xc"), "z");
+    EXPECT_EQ(StringUtil::substringAfter("zxcv", "zx"), "cv");
+
+    EXPECT_EQ(StringUtil::ends_with("asdf", "df"), true);
+    EXPECT_EQ(StringUtil::ends_with("asdf", "as"), false);
 }
 
 int main(int argc, char** argv) {
